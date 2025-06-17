d8bda569eb29443d70416068de11a16bae557ef5
farit2233 <farit2233@gmail.com>
2025-06-17 16:44:44 +0700

    edit css box admin home

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -8,8 +8,10 @@ require_once('sess_auth.php');
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo $_settings->info('title') != false ? $_settings->info('title') . ' | ' : '' ?><?php echo $_settings->info('name') ?></title>
   <link rel="icon" href="<?php echo validate_image($_settings->info('logo')) ?>" />
-  <!-- Google Font: Source Sans Pro -->
-  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
+  <!-- Google Font: Kanit -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url ?>plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
@@ -48,6 +50,11 @@ require_once('sess_auth.php');
       to {
         opacity: 1
       }
+    }
+
+    body {
+      font-family: "Kanit", sans-serif;
+      background-color: #FAFAFA;
     }
 
     .chartjs-render-monitor {
