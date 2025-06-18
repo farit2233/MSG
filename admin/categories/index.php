e8e78478d756b0a294a026b68c67044147fec444
farit2233 <farit2233@gmail.com>
2025-06-18 11:00:57 +0700

    update categories admin

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -1,25 +1,25 @@
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
+<?php if ($_settings->chk_flashdata('success')): ?>
+	<script>
+		alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+	</script>
+<?php endif; ?>
 <style>
-	.category-logo{
-		width:3em;
-		height:3em;
-		object-fit:cover;
-		object-position:center center;
+	.category-logo {
+		width: 3em;
+		height: 3em;
+		object-fit: cover;
+		object-position: center center;
 	}
 </style>
 <div class="card card-outline rounded-0 card-dark">
 	<div class="card-header">
-		<h3 class="card-title">List of Categories</h3>
+		<h3 class="card-title text-bold">หมวดหมู่สินค้าทั้งหมด</h3>
 		<div class="card-tools">
-			<a href="./?page=categories/manage_category" id="create_new" class="btn btn-flat btn-dark"><span class="fas fa-plus"></span>  Create New</a>
+			<a href="./?page=categories/manage_category" id="create_new" class="btn btn-flat btn-dark"><span class="fas fa-plus"></span> สร้างหมวดหมู่ใหม่</a>
 		</div>
 	</div>
 	<div class="card-body">
-        <div class="container-fluid">
+		<div class="container-fluid">
 			<table class="table table-hover table-striped table-bordered" id="list">
 				<colgroup>
 					<col width="5%">
@@ -29,46 +29,48 @@
 					<col width="15%">
 					<col width="10%">
 				</colgroup>
-				<thead>
+				<thead class="text-center">
 					<tr>
-						<th>#</th>
-						<th>Date Created</th>
-						<th>Name</th>
-						<th>Description</th>
-						<th>Status</th>
-						<th>Action</th>
+						<th>ที่</th>
+						<th>วันที่สร้าง</th>
+						<th>ชื่อหมวดหมู่</th>
+						<th>รายละเอียดหมวดหมู่</th>
+						<th>สถานะ</th>
+						<th>จัดการ</th>
 					</tr>
 				</thead>
 				<tbody>
-					<?php 
+					<?php
 					$i = 1;
-						$qry = $conn->query("SELECT * from `category_list` where delete_flag = 0 order by `name` asc ");
-						while($row = $qry->fetch_assoc()):
+					$qry = $conn->query("SELECT * from `category_list` where delete_flag = 0 order by `name` asc ");
+					while ($row = $qry->fetch_assoc()):
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
-							<td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
+							<td><?php echo date("Y-m-d H:i", strtotime($row['date_created'])) ?></td>
 							<td class=""><?= $row['name'] ?></td>
-							<td><p class="mb-0 truncate-1"><?php echo ($row['description']) ?></p></td>
+							<td>
+								<p class="mb-0 truncate-1"><?php echo ($row['description']) ?></p>
+							</td>
 							<td class="text-center">
-                                <?php if($row['status'] == 1): ?>
-                                    <span class="badge badge-success px-3 rounded-pill">Active</span>
-                                <?php else: ?>
-                                    <span class="badge badge-danger px-3 rounded-pill">Inactive</span>
-                                <?php endif; ?>
-                            </td>
-							<td align="center">
-								 <button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-				                  		Action
-				                    <span class="sr-only">Toggle Dropdown</span>
-				                  </button>
-				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item" href="./?page=categories/view_category&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item" href="./?page=categories/manage_category&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-				                  </div>
+								<?php if ($row['status'] == 1): ?>
+									<span class="badge badge-success px-3 rounded-pill">กำลังใช้งาน</span>
+								<?php else: ?>
+									<span class="badge badge-danger px-3 rounded-pill">ไม่ได้ใช้งาน</span>
+								<?php endif; ?>
+							</td>
+							<td class="text-center">
+								<button type="button" class="btn btn-flat p-1 btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+									จัดการ
+									<span class="sr-only">Toggle Dropdown</span>
+								</button>
+								<div class="dropdown-menu" role="menu">
+									<a class="dropdown-item" href="./?page=categories/view_category&id=<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> ดู</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item" href="./?page=categories/manage_category&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> แก้ไขหมวดหมู่</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> ลบหมวดหมู่</a>
+								</div>
 							</td>
 						</tr>
 					<?php endwhile; ?>
@@ -78,35 +80,39 @@
 	</div>
 </div>
 <script>
-	$(document).ready(function(){
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this category permanently?","delete_category",[$(this).attr('data-id')])
+	$(document).ready(function() {
+		$('.delete_data').click(function() {
+			_conf("Are you sure to delete this category permanently?", "delete_category", [$(this).attr('data-id')])
 		})
 		$('.table').dataTable({
-			columnDefs: [
-					{ orderable: false, targets: [5] }
-			],
-			order:[0,'asc']
+			columnDefs: [{
+				orderable: false,
+				targets: [5]
+			}],
+			order: [0, 'asc']
 		});
 		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
 	})
-	function delete_category($id){
+
+	function delete_category($id) {
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_category",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
+			url: _base_url_ + "classes/Master.php?f=delete_category",
+			method: "POST",
+			data: {
+				id: $id
+			},
+			dataType: "json",
+			error: err => {
 				console.log(err)
-				alert_toast("An error occured.",'error');
+				alert_toast("An error occured.", 'error');
 				end_loader();
 			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
+			success: function(resp) {
+				if (typeof resp == 'object' && resp.status == 'success') {
 					location.reload();
-				}else{
-					alert_toast("An error occured.",'error');
+				} else {
+					alert_toast("An error occured.", 'error');
 					end_loader();
 				}
 			}
