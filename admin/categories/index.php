5cf59b0649392a16396f64efd516b8ec65d0a7ec
farit2233 <farit2233@gmail.com>
2025-07-23 10:08:33 +0700

    fix color edit dropdown

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -82,7 +82,7 @@
 										<span class="sr-only">Toggle Dropdown</span>
 									</button>
 									<div class="dropdown-menu" role="menu">
-										<a class="dropdown-item" href="./?page=categories/manage_category&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-black"></span> แก้ไขหมวดหมู่</a>
+										<a class="dropdown-item" href="./?page=categories/manage_category&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-dark"></span> แก้ไขหมวดหมู่</a>
 										<div class="dropdown-divider"></div>
 										<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> ลบหมวดหมู่</a>
 									</div>
--- admin/products/index.php
+++ admin/products/index.php
@@ -100,7 +100,7 @@
 										<span class="sr-only">Toggle Dropdown</span>
 									</button>
 									<div class="dropdown-menu" role="menu">
-										<a class="dropdown-item" href="./?page=products/manage_product&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-black"></span> แก้ไขสินค้า</a>
+										<a class="dropdown-item" href="./?page=products/manage_product&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-dark"></span> แก้ไขสินค้า</a>
 										<div class="dropdown-divider"></div>
 										<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> ลบสินค้า</a>
 									</div>
