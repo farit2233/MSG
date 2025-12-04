5a718289e52475c651f5b7405437778f622d0edc
farit2233 <farit2233@gmail.com>
2025-12-04 17:18:11 +0700

    fix cart_list

--- cart_list.php
+++ cart_list.php
@@ -179,9 +179,6 @@ if ($_settings->userdata('id') != '' && $_settings->userdata('login_type') == 2)
                         <span>ส่วนลดสินค้า</span>
                         <span>-<span id="summary-discount-total">0.00</span> บาท</span>
                     </div>
-
-                    <hr>
-
                     <div class="summary-row total d-flex justify-content-between font-weight-bold" style="font-size: 1.2rem;">
                         <span>ยอดสุทธิ</span>
                         <span><span id="selected-total-desktop">0.00</span> บาท</span>
